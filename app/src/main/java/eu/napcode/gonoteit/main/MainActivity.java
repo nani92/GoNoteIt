1201bcb551df85e5cd31f41f11dc94fc4bbf387d
Natalia Jastrzębska <nataliak@10g.pl>
2018-04-25 20:33:04 +0200

    added view model

--- app/src/main/java/eu/napcode/gonoteit/main/MainActivity.java
+++ app/src/main/java/eu/napcode/gonoteit/main/MainActivity.java
@@ -1,7 +1,9 @@
-package eu.napcode.gonoteit;
+package eu.napcode.gonoteit.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import eu.napcode.gonoteit.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/eu/napcode/gonoteit/main/MainViewModel.java
@@ -0,0 +1,8 @@
+package eu.napcode.gonoteit.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    
+}
