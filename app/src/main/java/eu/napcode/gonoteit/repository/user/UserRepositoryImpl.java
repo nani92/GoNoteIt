122419aa1701100348718a8747f9d1b91f8fa6db
Natalia Jastrzębska <nataliak@10g.pl>
2018-05-04 17:48:43 +0200

    created mutation for aythentication and first test fir user repo

--- app/src/main/java/eu/napcode/gonoteit/repository/user/UserRepositoryImpl.java
+++ app/src/main/java/eu/napcode/gonoteit/repository/user/UserRepositoryImpl.java
@@ -1,15 +1,22 @@
 package eu.napcode.gonoteit.repository.user;
 
+import com.apollographql.apollo.ApolloClient;
+
 import javax.inject.Inject;
+
+import eu.napcode.gonoteit.AuthenticateMutation;
 
 public class UserRepositoryImpl implements UserRepository {
 
+    private ApolloClient apolloClient;
+
     @Inject
-    public UserRepositoryImpl() {
+    public UserRepositoryImpl(ApolloClient apolloClient) {
+        this.apolloClient = apolloClient;
     }
 
     @Override
     public void authenticateUser(String login, String password) {
-
+        apolloClient.mutate(new AuthenticateMutation(login, password));
     }
 }
--- app/src/test/java/eu/napcode/gonoteit/repository/user/UserRepositoryTest.java
+++ app/src/test/java/eu/napcode/gonoteit/repository/user/UserRepositoryTest.java
@@ -1,18 +1,38 @@
 package eu.napcode.gonoteit.repository.user;
 
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.ApolloMutationCall;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import eu.napcode.gonoteit.AuthenticateMutation;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserRepositoryTest {
+
+    @Mock
+    ApolloClient apolloClient;
+
+    @Mock
+    ApolloCall<AuthenticateMutation.Data> authenticateMutationApolloCall;
 
     private UserRepository userRepository;
 
     @Before
     public void initial() {
-        userRepository = new UserRepositoryImpl();
+        userRepository = new UserRepositoryImpl(apolloClient);
+    }
+
+    @Test
+    public void testAuthenticateIsCalled() {
+        userRepository.authenticateUser("", "");
+
+        Mockito.verify(apolloClient).mutate(Mockito.any(AuthenticateMutation.class));
     }
 }
