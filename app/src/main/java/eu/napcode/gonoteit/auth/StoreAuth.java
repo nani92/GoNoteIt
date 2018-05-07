a70ad5eebdb59f01c49debe168077cc12870dfc8
Natalia Jastrzębska <nataliak@10g.pl>
2018-05-07 14:47:03 +0200

    added timber for logging purposes

--- app/src/main/java/eu/napcode/gonoteit/auth/StoreAuth.java
+++ app/src/main/java/eu/napcode/gonoteit/auth/StoreAuth.java
@@ -5,6 +5,8 @@ import android.util.Log;
 
 import javax.inject.Inject;
 
+import timber.log.Timber;
+
 public class StoreAuth {
 
     private static final String USER_NAME_KEY = "user";
@@ -18,12 +20,14 @@ public class StoreAuth {
     }
 
     public void saveName(String name) {
+        Timber.d("Storing name %s", name);
         sharedPreferences.edit()
                 .putString(USER_NAME_KEY, name)
                 .apply();
     }
 
     public void saveToken(String token) {
+        Timber.d("Storing token %s", token);
         sharedPreferences.edit()
                 .putString(TOKEN_KEY, token)
                 .apply();
