bfaa0a2807c140e72d8a877802f3eefbb8fab457
Natalia Jastrzębska <nataliak@10g.pl>
2018-05-17 12:21:59 +0200

    dao an entity§

--- app/src/main/java/eu/napcode/gonoteit/dao/NoteDao.java
+++ app/src/main/java/eu/napcode/gonoteit/dao/NoteDao.java
@@ -1,7 +1,15 @@
 package eu.napcode.gonoteit.dao;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Query;
+
+import io.reactivex.Flowable;
+
+import static eu.napcode.gonoteit.dao.NoteEntity.TABLE_NAME;
 
 @Dao
 public interface NoteDao {
+
+    @Query("SELECT * FROM " + TABLE_NAME)
+    Flowable<NoteEntity> getAllNoteEntities();
 }
--- app/src/main/java/eu/napcode/gonoteit/dao/NoteEntity.java
+++ app/src/main/java/eu/napcode/gonoteit/dao/NoteEntity.java
@@ -1,7 +1,10 @@
 package eu.napcode.gonoteit.dao;
 
+import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
+
+import javax.annotation.Nullable;
 
 import static eu.napcode.gonoteit.dao.NoteEntity.TABLE_NAME;
 
@@ -10,6 +13,45 @@ public class NoteEntity {
 
     public static final String TABLE_NAME = "notes";
 
+    public static final String COLUMN_ID = "ID";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_CONTENT = "content";
+
     @PrimaryKey
-    int id;
+    @ColumnInfo(name = COLUMN_ID)
+    String id;
+
+    @Nullable
+    @ColumnInfo(name = COLUMN_TITLE)
+    private String title;
+
+    @Nullable
+    @ColumnInfo(name = COLUMN_CONTENT)
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Nullable
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@Nullable String title) {
+        this.title = title;
+    }
+
+    @Nullable
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(@Nullable String content) {
+        this.content = content;
+    }
 }
