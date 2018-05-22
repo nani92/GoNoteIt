280abb3a34e8e0299b04dfba7e370725de99994a
Natalia Jastrzębska <nataliak@10g.pl>
2018-05-22 19:04:16 +0200

    added add note widget

---
+++ app/src/main/java/eu/napcode/gonoteit/ui/widget/AddNoteWidgetProvider.java
@@ -0,0 +1,36 @@
+package eu.napcode.gonoteit.ui.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import eu.napcode.gonoteit.R;
+import eu.napcode.gonoteit.ui.main.MainActivity;
+
+public class AddNoteWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        //TODO change to create activity
+        ComponentName thisWidget = new ComponentName(context, AddNoteWidgetProvider.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+
+        for (int widgetId : allWidgetIds) {
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.add_note_widget_layout);
+            Intent intent = new Intent(context, MainActivity.class);
+
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            remoteViews.setOnClickPendingIntent(R.id.widget_createNoteButton, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+}
