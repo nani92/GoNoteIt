d1e126c2c1bf343cc5e7c95cbf369ba5d06fa411
Natalia Jastrzębska <natalia@napcode.eu>
2018-06-12 12:30:40 +0200

    displatying about fragment

---
+++ app/src/main/java/eu/napcode/gonoteit/ui/about/AboutFragment.java
@@ -0,0 +1,26 @@
+package eu.napcode.gonoteit.ui.about;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import eu.napcode.gonoteit.R;
+import eu.napcode.gonoteit.databinding.FragmentAboutBinding;
+
+public class AboutFragment extends Fragment {
+
+    private FragmentAboutBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false);
+
+        return binding.getRoot();
+    }
+}
