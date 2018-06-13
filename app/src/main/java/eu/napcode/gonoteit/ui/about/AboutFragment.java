ebec1087d11293b01ea15708536ef6f0143b811d
Natalia Jastrzębska <natalia@napcode.eu>
2018-06-13 13:17:07 +0200

    animating first about cardview

--- app/src/main/java/eu/napcode/gonoteit/ui/about/AboutFragment.java
+++ app/src/main/java/eu/napcode/gonoteit/ui/about/AboutFragment.java
@@ -1,16 +1,22 @@
 package eu.napcode.gonoteit.ui.about;
 
+import android.animation.LayoutTransition;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v4.app.Fragment;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import eu.napcode.gonoteit.R;
 import eu.napcode.gonoteit.databinding.FragmentAboutBinding;
+
+import static android.view.View.GONE;
 
 public class AboutFragment extends Fragment {
 
@@ -22,5 +28,51 @@ public class AboutFragment extends Fragment {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false);
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setupViews();
+    }
+
+    private void setupViews() {
+        setupLayoutTransitions();
+
+        binding.aboutCardView.setOnClickListener(v -> toggleAbout());
+    }
+
+    private void setupLayoutTransitions() {
+        LayoutTransition layoutTransition = new LayoutTransition();
+        layoutTransition.setDuration(getResources().getInteger(R.integer.anim_duration_medium));
+        layoutTransition.enableTransitionType(LayoutTransition.CHANGING);
+        binding.constraintLayout.setLayoutTransition(layoutTransition);
+
+    }
+
+    private void toggleAbout() {
+
+        if (binding.aboutIncluded.contentTextView.getVisibility() == GONE) {
+            expandLayout(binding.aboutIncluded.constraintLayout, R.layout.about_card_about_expanded);
+        } else {
+            collapseLayout(binding.aboutIncluded.constraintLayout, R.layout.about_card_about);
+        }
+    }
+
+    private void expandLayout(ConstraintLayout layoutToExpand, int expandLayoutId) {
+        ConstraintSet expandedConstraintSet = new ConstraintSet();
+        expandedConstraintSet.clone(getContext(), expandLayoutId);
+        expandedConstraintSet.applyTo(layoutToExpand);
+
+        TransitionManager.beginDelayedTransition(layoutToExpand);
+    }
+
+    private void collapseLayout(ConstraintLayout layoutToCollapse, int collapseLayoutId) {
+        ConstraintSet expandedConstraintSet = new ConstraintSet();
+        expandedConstraintSet.clone(getContext(), collapseLayoutId);
+        expandedConstraintSet.applyTo(layoutToCollapse);
+
+        TransitionManager.beginDelayedTransition(layoutToCollapse);
     }
 }
