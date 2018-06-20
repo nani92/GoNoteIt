b5a460965cd7eb70e46e45dd66c549c173b0aa87
Natalia Jastrzębska <natalia@napcode.eu>
2018-06-20 13:30:35 +0200

    removed all

--- app/src/main/java/eu/napcode/gonoteit/api/ApiConsts.java
+++
@@ -1,5 +0,0 @@
-package eu.napcode.gonoteit.api;
-
-public class ApiConsts {
-    public static final String API_URL = "http://10.0.0.105:8000/graphql/";
-}
--- app/src/main/java/eu/napcode/gonoteit/app/utils/InputValidator.java
+++
@@ -1,8 +0,0 @@
-package eu.napcode.gonoteit.app.utils;
-
-public class InputValidator {
-
-    public boolean isInputNotEmpty(String input) {
-        return input != null && input.length() > 0;
-    }
-}
