3436a34ba4a21583c3b210559876e2e3cf5405de
Natalia Jastrzębska <natalia@napcode.eu>
2018-06-27 10:42:02 +0200

    changed ip

--- app/src/main/java/eu/napcode/gonoteit/api/ApiConsts.java
+++ app/src/main/java/eu/napcode/gonoteit/api/ApiConsts.java
@@ -1,5 +1,5 @@
 package eu.napcode.gonoteit.api;
 
 public class ApiConsts {
-    public static final String API_URL = "http://10.0.0.105:8000/graphql/";
+    public static final String API_URL = "http://178.128.173.23:8000/graphql/";
 }
