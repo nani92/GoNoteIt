47e27dcd2228d2b9d6eb7a48bcce30712b1f5e2e
Natalia Jastrzębska <natalia@napcode.eu>
2018-07-16 15:00:20 +0200

    added enums fot perms

---
+++ app/src/main/java/eu/napcode/gonoteit/model/note/NoteModel.kt
@@ -0,0 +1,48 @@
+package eu.napcode.gonoteit.model.note
+
+import eu.napcode.gonoteit.dao.NoteEntity
+
+open class NoteModel {
+
+    var uuid: String? = null
+    var title: String? = null
+    var content: String? = null
+    var id: Long? = null
+    var imageBase64: String? = null
+    var updatedAt: Long? = null
+
+    constructor()
+
+    constructor(noteEntity: NoteEntity) {
+        this.uuid = noteEntity.uuid
+        this.title = noteEntity.title
+        this.content = noteEntity.content
+        this.id = noteEntity.id
+        this.imageBase64 = noteEntity.imageBase64
+        this.updatedAt = noteEntity.updatedAt
+    }
+
+    override fun equals(obj: Any?): Boolean {
+        return obj is NoteModel &&
+                uuid == obj.uuid &&
+                id == obj.id &&
+                areStringsEqual(title, obj.title) &&
+                areStringsEqual(content, obj.content) &&
+                areStringsEqual(imageBase64, obj.imageBase64)
+    }
+
+    private fun areStringsEqual(s: String?, s2: String?): Boolean {
+        return s == null && s2 == null || s == s2
+    }
+
+    enum class ReadPerms {
+        PUBLIC,
+        PRIVATE,
+        VIA_LINK
+    }
+
+    enum class WritePerms {
+        ONLY_OWNER,
+        EVERYONE
+    }
+}
--- app/src/main/java/eu/napcode/gonoteit/ui/create/PermissionsDialogFragment.kt
+++ app/src/main/java/eu/napcode/gonoteit/ui/create/PermissionsDialogFragment.kt
@@ -4,9 +4,6 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import eu.napcode.gonoteit.R
 
 class PermissionsDialogFragment : DialogFragment() {
