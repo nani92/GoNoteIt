a4bfb1918bc88559e73f8a8ac6f1f7269c68e41b
Natalia Jastrzębska <natalia@napcode.eu>
2018-07-16 13:47:20 +0200

    added view for perms dialog

--- app/src/main/java/eu/napcode/gonoteit/ui/create/PermisionsDialogFragment.kt
+++ app/src/main/java/eu/napcode/gonoteit/ui/create/PermisionsDialogFragment.kt
@@ -1,7 +1,15 @@
 package eu.napcode.gonoteit.ui.create
 
-import android.app.DialogFragment
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import eu.napcode.gonoteit.R
 
 class PermisionsDialogFragment : DialogFragment() {
 
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val view =  activity!!.layoutInflater.inflate(R.layout.dialog_privacy, null)
+
+        return super.onCreateDialog(savedInstanceState)
+    }
 }
