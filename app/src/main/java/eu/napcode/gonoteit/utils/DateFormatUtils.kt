c9809872a3d3c6317f3833fec30353bf0ddf5ad1
Natalia Jastrzębska <natalia@napcode.eu>
2019-03-27 19:34:33 +0100

    displaying day name

--- app/src/main/java/eu/napcode/gonoteit/utils/DateFormatUtils.kt
+++ app/src/main/java/eu/napcode/gonoteit/utils/DateFormatUtils.kt
@@ -7,7 +7,7 @@ public var dateFormatWithTime =
         SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault())
 
 public var dateFormat =
-        SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+        SimpleDateFormat("EEEE, dd.MM.yyyy", Locale.getDefault())
 
 
 public var timeFormat =
